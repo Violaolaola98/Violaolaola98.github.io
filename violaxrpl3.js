44000eb1b2a9623073c936ea0a24ea7ede26e155
Violaolaola98 <168519256+Violaolaola98@users.noreply.github.com>
2024-04-30 14:41:04 +0700

    Add files via upload

---
+++ violaxrpl3.js
@@ -0,0 +1,170 @@
+var i=5
+var withoutternary;
+
+if(i>10){
+   withoutternary = "greater than 10";
+   }else{
+   withoutternary = "less than 10"
+   }
+   const ternaryanswer = i > 10? "greater than 10": "less than 10";
+   const ternarywithif = i > 10? "greater than 10" : i < 5? "less than 5" :"between 5 and 10";
+   console.log(withoutternary)
+   console.log(ternaryanswer)
+   console.log(ternarywithif)
+   
+   var i= 11;
+ var withoutternary;
+ 
+ if (i>33){
+      withoutternaary = "greater than 10";
+ } else {
+      withoutternaary = "less than 10"
+ }
+ 
+ var ternaryaanswer1 = i > 33 ? "greater than 33": "less than 33";
+ var ternaarywithif = i > 33? "greater than 11" : i < 11 ? "less than 11" :"between 11 and 33";
+console.log(withoutternaary)
+console.log(ternaryaanswer1)
+console.log(ternaarywithif)
+
+var ternaryaanswer2 = i > 55 ? "greater than 55": "less than 55";
+ var ternaarywithif = i > 55? "greater than 18" : i < 18 ? "less than 18" :"between 18 and 55";
+console.log(withoutternaary)
+console.log(ternaryaanswer2)
+console.log(ternaarywithif)
+
+var ternaryaanswer3 = i >  40 ? "greater than 40": "less than 40";
+ var ternaarywithif = i > 40? "greater than 24" : i < 24 ? "less than 24" :"between 11 and 40";
+console.log(withoutternaary)
+console.log(ternaryaanswer3)
+console.log(ternaarywithif)
+
+var ternaryaanswer4 = i > 88 ? "greater than 88": "less than 88";
+ var ternaarywithif = i > 88? "greater than 20" : i < 20 ? "less than 20" :"between 20 and 88";
+console.log(withoutternaary)
+console.log(ternaryaanswer4)
+console.log(ternaarywithif)
+
+var ternaryaanswer5 = i > 66 ? "greater than 10": "less than 66";
+ var ternaarywithif = i > 66? "greater than 10" : i < 10 ? "less than 10" :"between 10 and 66";
+console.log(withoutternaary)
+console.log(ternaryaanswer5)
+console.log(ternaarywithif)
+
+  var suhu = 30;
+  var cuaca = 19;
+  if (suhu > cuaca) {
+  console.log("suhu panas");
+  }
+  else if (suhu === maxcuaca)
+  {
+  console.log("cuaca standar");
+ } 
+ else 
+ {
+ console.log("cuaca dingin");
+ }
+ 
+ var badan = 45;
+ var maxbadan = 55;
+ if(badan > maxbadan){
+ console.log("obesitas");
+ }
+ else if (badan === maxbadan)
+ {
+ console.log("badan ideal");
+ }
+ else
+ {
+ console.log("badan baik");
+  }
+  
+  var rambut = 10;
+  var maxpotong = 15;
+  if(rambut > maxpotong){
+  console.log("anak kecil");
+  }
+  else if (rambut === maxpotong)
+  {
+  console.log("anak sekolah");
+  }
+  else
+  {
+  console.log("orang dewasa");
+  }
+  
+  var makan = 2;
+  var maxmakan = 3;
+  if(makan > maxmakan){
+  console.log("kebanyakan makan");
+  }
+  else if (makan === maxmakan)
+  {
+  console.log("standar");
+  }
+  else
+  {
+  console.log("kekurangan makan");
+  }
+  
+  var minum = 80;
+  var maxminum = 120;
+  if(minum > maxminum){
+  console.log("kebanyakan minum");
+  }
+  else if (minum === maxminum)
+  {
+  console.log("standar");
+  }
+  else
+  {
+  console.log("kekurangan minum");
+  }
+  
+  var shift = 7.00;
+  var maxshift = 7.15;
+  if(shift > maxshift){
+  console.log("gak terlambat");
+  }
+  else if(shift === maxshift)
+  {
+  console.log("standar");
+  }
+  else
+  {
+  console.log("terlambat");
+  }
+  var no = 10;
+  var operator = no === 10 ?"sama dengan":
+                 no > 10 ?"lebih besar":
+                 no < 10 ?"lebih kecil":
+                 "gak tahu"
+       console.log(operator);
+       
+  var no = 18;
+  var operator = no === 18 ?"sama dengan":
+                 no > 18 ?"lebih besar":
+                 no < 18 ?"lebih kecil":
+                 "gak tahu"
+       console.log(operator);
+       
+  var no = 30;
+  var operator = no === 30 ?"sama dengan":
+                 no > 30 ?"lebih besar":
+                 no < 30 ?"lebih kecil":
+                 "gak tahu"
+       console.log(operator);
+       
+  var no = 77;
+  var operator = no === 77 ?"sama dengan":
+                 no > 77 ?"lebih besar":
+                 no < 77 ?"lebih kecil":
+                 "gak tahu"
+       console.log(operator);
+       
+  var no = 99;
+  var operator = no === 99 ?"sama dengan":
+                 no > 99 ?"lebih besar":
+                 no < 99 ?"lebih kecil":
+                 "gak tahu"
+       console.log(operator);
